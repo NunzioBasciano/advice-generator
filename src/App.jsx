4665fb89e6ac38773edaea9981cc16cdf1cf49c9
NunzioBasciano <nbasho1988@gmail.com>
2024-07-15 14:51:13 +0200

    add: first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card/Card.jsx'
+import adviceArray from './components/data/advice.jsx'
+
+function App() {
+
+  const [message, setMessage] = useState(1);
+
+  function handleClick(e) {
+    if (e.target.id) {
+      setMessage(randomNumber())
+    }
+  }
+
+  function randomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  const currentAdvice = adviceArray.find(advice => advice.id === message);
+  const title = currentAdvice.id;
+  const description = currentAdvice.advice;
+
+  return (
+
+    <Card
+      title={title}
+      description={description}
+      onClick={handleClick}
+    />
+  )
+}
+
+export default App
